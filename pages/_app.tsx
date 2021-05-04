228a9394683eb77ead047abe8262eee5ae9e3785
yaytomato <yehoon_s@naver.com>
2021-05-04 17:56:05 +0900

    build: add react-redux + redux toolkit for state management

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import { Provider } from "react-redux";
+
+import store from "store";
 import "../styles/globals.css";
 
 const ChlngersWeb = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 };
 
 export default ChlngersWeb;
